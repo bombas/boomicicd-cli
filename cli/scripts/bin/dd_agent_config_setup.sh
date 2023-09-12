6a7f8991d29b8bb672930057ade79036a5e1d5b7
Srini <srinivasula.y@gmail.com>
2023-09-12 19:49:13 +0530

    updating the bombas metrics

--- cli/scripts/bin/dd_agent_config_setup.sh
+++ cli/scripts/bin/dd_agent_config_setup.sh
@@ -6,14 +6,14 @@ export DD_SITE="datadoghq.com"
 #bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script.sh)"
 bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
 
-source /home/boomi/.profile
+. /home/boomi/.profile
 cat <<EOF >> /etc/datadog-agent/datadog.yaml
 tags:
- - client:  ${client}
- - brand:  ${brand}
- - environment: ${environment}
- - pod_name:  ${ATOM_LOCALHOSTID}
- - container_name:  ${BOOMI_CONTAINERNAME}
+ - client:${client}
+ - group:${group}
+ - environment:${environment}
+ - pod_name:${ATOM_LOCALHOSTID}
+ - container_name:${BOOMI_CONTAINERNAME}
 EOF
 
 cat <<EOF > /etc/datadog-agent/conf.d/process.d/conf.yaml
@@ -21,15 +21,9 @@ cat <<EOF > /etc/datadog-agent/conf.d/process.d/conf.yaml
 init_config:
 
 instances:
-- name: ${BOOMI_CONTAINERNAME}
+- name:${BOOMI_CONTAINERNAME}
   search_string: ['com.boomi.container.core.Container']
   exact_match: False
-  tags:
-  - "client:${client}"
-  - "brand:${brand}"
-  - "environment:${environment}"
-  - "pod_name:${ATOM_LOCALHOSTID}"
-  - "container_name:${BOOMI_CONTAINERNAME}"
 EOF
 
 chown dd-agent:dd-agent /etc/datadog-agent/conf.d/process.d/conf.yaml
@@ -41,23 +35,21 @@ init_config:
   collect_default_metrics: true
   java_bin_path: /usr/bin/java
   conf:
-  - include:
+    - include:
         domain: com.boomi.container.services
         type:
-        - ExecutionManager
-        - MessageQueue
-        - ResourceManager
-        - Scheduler
+          - ExecutionManager
+          - MessageQueue
+          - ResourceManager
+          - Scheduler
+    - include:
+        domain: java.lang
+        attribute:
+          - SystemLoadAverage
 instances:
   - host: 127.0.0.1
     port: 5003
-    name: ${BOOMI_CONTAINERNAME}
-    tags:
-    - "client:${client}"
-    - "brand:${brand}"
-    - "environment:${environment}"
-    - "pod_name:${ATOM_LOCALHOSTID}"
-    - "container_name:${BOOMI_CONTAINERNAME}"
+    name:${BOOMI_CONTAINERNAME}
 EOF
 
 chown dd-agent:dd-agent /etc/datadog-agent/conf.d/jmx.d/conf.yaml
