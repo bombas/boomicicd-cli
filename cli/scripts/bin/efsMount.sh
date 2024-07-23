25ca77538684987d1afde39ccbc1eb7e26692d6c
srinivasulary <128830105+srinivasulary@users.noreply.github.com>
2024-07-23 18:41:18 +0530

    Update efsMount.sh

--- cli/scripts/bin/efsMount.sh
+++ cli/scripts/bin/efsMount.sh
@@ -38,10 +38,17 @@ sudo mkdir -p "${mountPoint}/boomi"
 
 ## update fstab
 if [ "${platform}" = "aws" ]; then
-	echo "${efsMount}.efs.${defaultAWSRegion}.amazonaws.com:/ $mountPoint nfs4 defaults,_netdev 0 0" | sudo tee -a /etc/fstab
+        export EC2_AVAIL_ZONE=`curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone`
+        export EC2_REGION="`echo \"$EC2_AVAIL_ZONE\" | sed 's/[a-z]$//'`"	
+        echo "mounting aws efs.."
+        sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 ${efsMount}.efs.${EC2_REGION}.amazonaws.com:/ /${mountPoint}	
+	# sudo mount -t efs -o tls ${efsMount}.efs.${EC2_REGION}.amazonaws.com:/ "${mountPoint}"
+	echo "${efsMount}.efs.${EC2_REGION}.amazonaws.com:/ $mountPoint nfs4 defaults,_netdev 0 0" | sudo tee -a /etc/fstab
 else
 	# GCP/Azure platforms
 	echo "mounting ${efsMount}..."
 	echo "${efsMount} $mountPoint nfs4 defaults,_netdev 0 0" | sudo tee -a /etc/fstab
 fi
+sudo chown $serviceUserName:$groupName "${mountPoint}"
 sudo mount -a
+# sudo chown -R $serviceUserName:$groupName "${mountPoint}"
